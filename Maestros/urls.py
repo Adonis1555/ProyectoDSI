84ca34f76da335aeb3fc28bb087d463bcfd174d0
Adonis1555 <148599515+Adonis1555@users.noreply.github.com>
2026-06-07 19:24:36 -0600

    pantalla de demeritos (estudiante)

--- Maestros/urls.py
+++ Maestros/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
         path('control_alumnos/',views.control_alumnos,name="control_alumnos"),
         path('registro_alumnos/',views.registrar_alumno,name="registro_alumnos"),
         path('control_demeritos/',views.control_demeritos,name="control_demeritos"),
+        path('registro_demeritos',views.registrar_demeritos,name="registrar_demeritos")
         
 ]
